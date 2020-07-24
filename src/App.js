34ddf4f4b0c3ad630266dd2d44f0429d161f124f
Warren Uhrich <hello@warren.codes>
2020-07-24 10:50:11 -0600

    Update App link text.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's Learn React
         </a>
       </header>
     </div>

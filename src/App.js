0ea0e29478111228014341dc83ae4ba6812dff20
Warren Uhrich <hello@warren.codes>
2020-07-24 13:44:00 -0600

    Trim some fat.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Hello from './Hello'; // Make sure to import your component if you're going to use it in any file!
 import HelloYourName from './HelloYourName';
 import HelloStudents from './HelloStudents';
